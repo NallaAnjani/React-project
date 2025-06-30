986382cbe6dc866ebfa1d9c38274bdda5306f96c
NallaAnjani <anjanikumarinalla@gmail.com>
2025-06-30 17:50:14 +0530

    msg11

--- src/Dashboards/UserDashboard/UserSidebar/MyBookings/MyBookings.jsx
+++ src/Dashboards/UserDashboard/UserSidebar/MyBookings/MyBookings.jsx
@@ -2,12 +2,8 @@ import { useState,useEffect } from "react";
 import { getDocs,collection } from "firebase/firestore";
 import { db } from "../../../../config_details/Config_details";
 const MyBookings=()=>{
-    const[events,setEvents] = useState([])
-    const[loading,setLoading] = useState(true)
-    useEffect=async()=>{
-           const data=await collection(db,"admins")
-           console.log(data)
-    }
+    
+    
     return(
         <div>My Bookings</div>
     );
